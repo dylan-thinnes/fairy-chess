4248f6021f6937130005dd9c083544296b524f88
Dylan Thinnes <dylan.thinnes@protonmail.com>
2019-11-26 18:42:06 +0000

    Comment / improve whitespace in Movement

--- src/Movement.purs
+++ src/Movement.purs
@@ -54,14 +54,15 @@ instance preconditionNextShow :: (Show location, Show piece)
     show (PositionsEqual x y _) = "PositionsEqual " <> show x <> " " <> show y <> " (and a function)"
     show Failure = "Failure"
 
+-- Precondition monad, made from Free monad from PreconditionNext
+-- Use a newtype so that Precondition monad can have MonadZero
 newtype Precondition location piece a = Precondition (Free (PreconditionNext location piece) a)
 derive instance preconditionNewtype :: Newtype (Precondition location piece a) _
+
+-- Synonym for Precondition with Boolean return value
 type Pre location piece = Precondition location piece Boolean
 
-liftPre :: forall location piece a. 
-           (PreconditionNext location piece a) -> Precondition location piece a
-liftPre = Precondition <<< liftF
-
+-- Instances to turn "Free PreconditionNext" to "Precondition" with MonadZero
 instance preconditionFunctor :: Functor (Precondition location piece) where
     map f = wrap <<< map f <<< unwrap
 instance preconditionApply :: Apply (Precondition location piece) where
@@ -82,6 +83,7 @@ instance preconditionPlus :: Plus (Precondition location piece) where
 instance preconditionAlternative :: Alternative (Precondition location piece)
 instance preconditionMonadZero :: MonadZero (Precondition location piece)
 
+-- PreconditionNext data constructors turned to Preconditions
 neverMoved              = wrap <<< toFreeBind1 NeverMoved
 atPiece                 = wrap <<< toFreeBind1 AtPiece
 unthreatenedLocation    = wrap <<< toFreeBind1 UnthreatenedLocation
@@ -90,12 +92,15 @@ occupiedBy              = wrap <<< toFreeBind1 OccupiedBy
 movementHistoryComplete = wrap   $ toFreeBind0 MovementHistoryComplete
 movementHistoryPiece    = wrap <<< toFreeBind1 MovementHistoryPiece
 positionsEqual x        = wrap <<< toFreeBind1 (PositionsEqual x)
-failure                 = wrap $ liftF Failure
+failure                 = wrap   $ liftF Failure
 
+-- Project Maybe a to m a, where m is MonadZero
+-- Always turn Just a to pure a, Nothing to empty
 try :: forall m a. (MonadZero m) => Maybe a -> m a
 try Nothing  = empty
 try (Just x) = pure x
 
+-- Simple data type expressing all possible fundamental actions in a game
 data GameAction location piece
     = Move piece (AbsOrRel location)
     | Promote piece (Array piece)
@@ -103,40 +108,57 @@ data GameAction location piece
     | Clear (AbsOrRel location)
 
 data QuickMoveSpec location piece
+      -- Finish by executing these actions, and then ensure postconds
     = Finally (Array (GameAction location piece))
               (Pre location piece)
+      -- Require the following preconditions before trying the following move spec
     | Require (Pre location piece)
               (QuickMoveSpec location piece)
     | Choice (Array (QuickMoveSpec location piece))
+      -- If the first succeeds, the second *must* be done
     | Sequence (QuickMoveSpec location piece) (QuickMoveSpec location piece)
+      -- If the given move spec fails, succeed with a noop
     | Optionally (QuickMoveSpec location piece)
+      -- Repeat the action until it fails
     | Repeat (QuickMoveSpec location piece)
 
-while :: forall location piece. Pre location piece -> QuickMoveSpec location piece -> QuickMoveSpec location piece
+while 
+    :: forall location piece. 
+       Pre location piece -> QuickMoveSpec location piece -> QuickMoveSpec location piece
 while cond action = Repeat (Require cond action)
 
-locEmpty :: forall location piece. (Eq piece) => AbsOrRel location -> Pre location piece
+locEmpty 
+    :: forall location piece. (Eq piece) => 
+       AbsOrRel location -> Pre location piece
 locEmpty loc = (_ == Nothing) <$> occupiedBy loc
 
-repeatMove :: forall location piece. (Eq piece) => piece -> AbsOrRel location -> QuickMoveSpec location piece
+repeatMove 
+    :: forall location piece. (Eq piece) => 
+       piece -> AbsOrRel location -> QuickMoveSpec location piece
 repeatMove self location = Repeat $ passiveMoveOnly self location
 
-passiveMoveOnly :: forall location piece. (Eq piece) => piece -> AbsOrRel location -> QuickMoveSpec location piece
+passiveMoveOnly
+    :: forall location piece. (Eq piece) => 
+       piece -> AbsOrRel location -> QuickMoveSpec location piece
 passiveMoveOnly self location
     = Require (locEmpty location) (Finally [Move self location] $ pure true)
 
-attackMoveOnly :: forall location name team. (Eq team) => Piece name team -> AbsOrRel location -> QuickMoveSpec location (Piece name team)
+attackMoveOnly
+    :: forall location name team. (Eq team) => 
+       Piece name team -> AbsOrRel location -> QuickMoveSpec location (Piece name team)
 attackMoveOnly self location 
     = Require (occupiedBy location >>= try >>= \occupant -> pure $ team occupant /= team self)
               (Finally [Clear location, Move self location] $ pure true)
 
-passiveOrAttackMove :: forall location name team. (Eq team) => Piece name team -> AbsOrRel location -> QuickMoveSpec location (Piece name team)
+passiveOrAttackMove 
+    :: forall location name team. (Eq team) => 
+       Piece name team -> AbsOrRel location -> QuickMoveSpec location (Piece name team)
 passiveOrAttackMove self location 
     = Finally [Clear location, Move self location] $ pure true
 
-repeatMoveUntilMaybeAttack :: forall location name team. 
-                              (Eq team) => (Eq name) => 
-                              Piece name team -> AbsOrRel location -> QuickMoveSpec location (Piece name team)
+repeatMoveUntilMaybeAttack
+    :: forall location name team. (Eq team) => (Eq name) => 
+       Piece name team -> AbsOrRel location -> QuickMoveSpec location (Piece name team)
 repeatMoveUntilMaybeAttack self location
     = Sequence (repeatMove self location) $ Optionally (attackMoveOnly self location)
 
@@ -171,5 +193,6 @@ sameType (Piece n1 t1 _) (Piece n2 t2 _) = n1 == n2 && t1 == t2
 data State piece board = State
 
 class IsPiece slot name team location | name -> name team location where
-    toMoveSpec :: (Eq name) => (Eq team) => slot -> (Piece name team) -> QuickMoveSpec location (Piece name team)
+    toMoveSpec :: (Eq name) => (Eq team) => 
+                  slot -> (Piece name team) -> QuickMoveSpec location (Piece name team)
     initialState :: slot -> Array (Tuple (Piece name team) location)
