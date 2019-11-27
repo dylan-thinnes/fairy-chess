44f79377ce3ad925f9b0d867a0fc137048a34c7a
Dylan Thinnes <dylan.thinnes@protonmail.com>
2019-11-27 10:44:26 +0000

    Implement general AbsOrRel resolver for IsBoard

--- src/Boards.purs
+++ src/Boards.purs
@@ -1,9 +1,14 @@
 module Boards where
 
+import Locations
+
 import Data.Maybe
 
 class IsBoard slot location | slot -> location where
     unrel :: slot -> location -> location -> Maybe location
     valid :: slot -> location -> Boolean
 
-
+resolve :: forall slot location. (IsBoard slot location) 
+        => slot -> location -> AbsOrRel location -> Maybe location
+resolve slot origin (Relative location) = unrel slot origin location
+resolve slot origin (Absolute location) = if valid slot location then Just location else Nothing
