ca3b0928fa85a4ba21827094511bc144f471932a
Dylan Thinnes <dylan.thinnes@protonmail.com>
2019-11-27 13:58:51 +0000

    Define a layout typeclass to go with boards

---
+++ src/Layouts.purs
@@ -0,0 +1,13 @@
+module Layouts where
+
+import Boards
+import Movement
+
+import Data.Maybe
+import Data.Tuple (Tuple)
+
+class ( IsPiece slot name team location, IsBoard slot location )
+     <= IsLayout slot name team location
+      | slot -> name team location where
+    initialState :: slot -> Array (Tuple (Piece name team) location)
+    isBackRank :: slot -> team -> location -> Boolean
